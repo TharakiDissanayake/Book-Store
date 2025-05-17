50285e4de18947d4df31175e191794a8e134e41e
TharakiDissanayake <tharutmd8@gmail.com>
2025-05-17 10:16:16 +0530

    Create responsive navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
   return (
     <>
-      <nav>NavBar</nav>
-      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+      <Navbar/>
+      <main className='min-h-screen px-4 py-6 mx-auto max-w-screen-2xl font-primary'>
       <Outlet/>
       </main>
       <footer>Footer</footer>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
